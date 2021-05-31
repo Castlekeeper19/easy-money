e9963fefb8b7b8798764ba5feafc9ac5d1df4255
Luke Chatelain <jens.chatelain@gmail.com>
2021-05-31 22:19:36 +0900

    added user account field and added sell stock function

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,18 +1,19 @@
 class Account < ApplicationRecord
   ACCOUNT_TYPES = ["Cash", "Retirement", "Brokerage","Liability"]
 
-  has_many :holdings
-  has_many :goals
+  has_many :holdings, dependent: :destroy
+  has_many :goals, dependent: :destroy
   has_many :stocks, through: :holdings
   validates :name, presence: true
   validates :account_type, inclusion: {in: ACCOUNT_TYPES}
+
 
   def account_balance
     @balance = 0
     self.holdings.each do |holding|
      @balance += holding.stock_balance
     end
-    self.balance = @balance.round(2)
+    self.balance = @balance.round(2) + self.cash_reserve.round(2)
     self.save
     self.balance
   end
@@ -25,4 +26,8 @@ class Account < ApplicationRecord
     sum.round(2)
   end
 
+  def stock_sold(holding)
+    self.cash_reserve += holding.stock_balance
+    self.save
+  end
 end
